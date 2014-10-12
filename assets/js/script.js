a42c311e7ed39cd5413b1e4d677ccd40a6d1b08d
Nick Clarkson <nikclarkson@gmail.com>
2014-10-12 22:00:37 +0100

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,136 +1,170 @@
+var helper = (function(){
 
-
-// http://dailydead.com/feed
-// http://www.bikeexif.com/feed
-// http://kickstart.bikeexif.com/wp-content/themes/bikeexif/images/random/bike_exif_logo_5b.jpg
-
-var feeds = [
- { url: 'http://dailydead.com/feed', slug: 'daily-dead' },
- { url: 'http://www.bikeexif.com/feed', slug: 'bike-exif' },
- { url: 'http://feeds.feedburner.com/edgarwrightherefeed', slug: 'edgar-wright-here' },
- { url: 'http://www.newstatesman.com/feeds_allsite/site_feed.php', slug: 'newstatesman' }
-];
-
-var entries = [];
-
-function setEntries(){
-  $.each(feeds, function(){
-    var feed = this;
-    $.each(feed.data.feed.entries, function(){
-      var post = this;
-          post.imUrl = '#/'+feed.slug+link(post);
-          post.imFeed = feed;
-          entries.push(post);
-    });
-  });
-
-  entries.sort(function(a,b){
-    return strftime(a.publishedDate) > strftime(b.publishedDate) ? -1 : 1;
-  });
-
-  // console.log(entries);
-
-}
-
-var $app = $('#app');
-
-var dispatcher = (function(){
-  var count = 0,
-      $loader = $('.loading span'),
-      loaded = false,
-      finished;
   return {
-    publish: function(data){
+
+    title: function(str){
+      document.title = str;
+    },
+
+    guid: function(item){
+
+      var guid = $('guid', item.xmlNode).text();
+
+      $.each(["\\/","http:","\\.","\\?","=","-","www"], function(){
+        var r = new RegExp(this, 'g');
+        guid = guid.replace(r, '');
+      });
+
+      return '/'+guid;
+
+    },
+
+    feedBySlug: function(slug, feeds){
+
+      var data = false;
 
       $.each(feeds, function(){
-        if(data.feed.feedUrl == this.url){
-          this.data = data;
+        if(slug == this.slug){
+          data = this;
           return true;
         }
       });
 
-      --count;
+      return data;
 
-      $loader.html((feeds.length-count) + ' / ' + feeds.length);
-      if(count <= 0){
-        setEntries();
-        loaded = true;
-        finished();
-      }
     },
-    subscribe: function(){
-      count++;
-    },
-    onLoad: function(method){
-      if(loaded) {
-        method();
-      } else {
-        finished = method;
-      }
+
+    postBySlugPost: function(slug, post, feeds){
+
+      var feed = helper.feedBySlug(slug, feeds), o = false;
+
+      $.each(feed.data.feed.entries, function(){
+
+        var guid = helper.guid(this);
+
+        if(guid == '/'+post){
+          o = this;
+          return true;
+        }
+
+      });
+
+      return {
+        feed: feed,
+        post: o
+      };
+
     }
+
   }
+
 })();
 
 
-google.load("feeds", "1");
+/////////////////////////////////////////
 
-function link(item){
-  console.log(item.xmlNode);
-  var guid = $('guid', item.xmlNode).text();
-  $.each(["\\/","http:","\\.","\\?","=","-","www"], function(){
-    var r = new RegExp(this, 'g');
-    guid = guid.replace(r, '');
-  });
-  return '/'+guid;
-}
+var loader = (function(feeds){
 
-function feedBySlug(slug){
-  var data = false;
+  var $el = $('.loading .progress').html('Loading 0 / ' + feeds.length),
+      count = feeds.length;
+
+  google.load("feeds", "1");
+
   $.each(feeds, function(){
-    if(slug == this.slug){
-      data = this;
-      return true;
-    }
+    var that = this;
+    google.setOnLoadCallback(function(){
+      var feed = new google.feeds.Feed(that.url);
+      feed.setNumEntries(100);
+      feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
+      feed.load(function(result) {
+        PubSub.publish('Feed Loaded', result);
+      });
+    });
   });
-  return data;
-}
 
-function postBySlugPost(slug, post){
-  var feed = feedBySlug(slug),
-      o = false;
+  PubSub.subscribe('Feed Loaded', function(msg, data){
 
-  $.each(feed.data.feed.entries, function(){
-    var guid = link(this);
-    if(guid == '/'+post){
-      o = this;
-      return true;
+    $.each(feeds, function(){
+      if(data.feed.feedUrl == this.url){
+        this.data = data;
+        return true;
+      }
+    });
+
+    --count;
+
+    $el.html('Loading ' + (feeds.length-count) + ' / ' + feeds.length);
+
+    if(count <= 0){
+      PubSub.publish('All Feeds Loaded', feeds);
     }
+
+  });
+
+  PubSub.subscribe('Page Built', function(msg, data){
+    setTimeout(function(){
+      $('.loading').fadeOut();
+    }, 1000)
   });
 
   return {
-    feed: feed,
-    post: o
-  };
-}
+    data: function(){
+      return (count <= 0) ? feeds : false;
+    }
+  }
 
-function four04(){
+})(feeds);
 
-}
+/////////////////////////////////////////
 
-$.each(feeds, function(){
-  var that = this;
-  google.setOnLoadCallback(function(){
-    dispatcher.subscribe();
-    var feed = new google.feeds.Feed(that.url);
-    feed.setNumEntries(100);
-    feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
-    feed.load(function(result) {
-      if (!result.error) {
-        dispatcher.publish(result);
-      }
-    });
+var ocn = (function(){
+  var $nav = $('#ocn nav');
+  PubSub.subscribe('All Feeds Loaded', function(msg, data){
+    var t = _.template($("#feed-menu").html());
+    $('#ocn nav').html(t({feeds:data}));
   });
-});
+})();
+
+/////////////////////////////////////////
+
+var entries = (function(){
+
+  var posts = [];
+
+  PubSub.subscribe('All Feeds Loaded', function(msg, data){
+    $.each(data, function(){
+      var feed = this;
+      $.each(feed.data.feed.entries, function(){
+        var post = this;
+            post.imUrl = '#/'+feed.slug+helper.guid(post);
+            post.imFeed = feed;
+
+            posts.push(post);
+      });
+    });
+
+    posts.sort(function(a,b){
+      var ad = new Date(a.publishedDate),
+          bd = new Date(b.publishedDate);
+
+      return ad.getTime() > bd.getTime() ? -1 : 1;
+
+    });
+
+    PubSub.publish('Entries Built', posts);
+
+  });
+
+  return {
+    data: function(){
+      return posts;
+    }
+  }
+
+})();
+
+
+/////////////////////////////////////////
 
 var AppRouter = Backbone.Router.extend({
   routes: {
@@ -142,43 +176,152 @@ var AppRouter = Backbone.Router.extend({
 
 var router = new AppRouter;
 
+/////////////////////////////////////////
+
 router.on('route:default', function(actions) {
-  dispatcher.onLoad(function(){
+
+  var view = function(data){
     var t = _.template($("#default").html());
-    $app.html(t({entries:entries}));
-  });
+    $('#app').html(t({entries:data}));
+    helper.title('Home | All posts');
+
+    PubSub.publish('Page Built', {});
+
+  }
+
+  if(!loader.data()){
+    PubSub.subscribe('Entries Built', function(msg, data){
+      view(data);
+    });
+  } else {
+    view(entries.data());
+  }
+
 });
+
+/////////////////////////////////////////
 
 router.on('route:index', function(slug) {
-  dispatcher.onLoad(function(){
-    var feed = feedBySlug(slug);
+
+  var view = function(data){
+
+    var feed = helper.feedBySlug(slug, data);
+
     if(feed){
       var t = _.template($("#list").html());
-      $app.html(t({feed:feed}));
+      $('#app').html(t({feed:feed}));
+      helper.title('Feed');
     } else {
-      four04();
+      console.log('problem');
     }
-  });
+
+    PubSub.publish('Page Built', {});
+
+  }
+
+  if(!loader.data()){
+    PubSub.subscribe('All Feeds Loaded', function(msg, data){
+      view(data);
+    });
+  } else {
+    view(loader.data());
+  }
+
 });
 
+/////////////////////////////////////////
+
 router.on('route:post', function(slug, post){
-  dispatcher.onLoad(function(){
+
+  var view = function(data){
     var t = _.template($("#post").html()),
-        p = postBySlugPost(slug, post);
+        p = helper.postBySlugPost(slug, post, data);
     if(p){
-      $app.html(t(p));
+      $('#app').html(t(p));
+      helper.title('Post');
     } else {
-      four04();
+
     }
-  });
+
+    PubSub.publish('Page Built', {});
+
+  }
+
+  if(!loader.data()){
+    PubSub.subscribe('All Feeds Loaded', function(msg, data){
+      view(data);
+    });
+  } else {
+    view(loader.data());
+  }
+
 });
+
+/////////////////////////////////////////
 
 Backbone.history.start();
 
-$('#app').on('click', function(e){
+/////////////////////////////////////////
+
+$(window).on('click', function(e){
   $target = $(e.target);
   if($target.hasClass('nav')){
     window.scrollTo(0,0);
     router.navigate($target.attr('href'), { trigger: true });
   }
+
+  if($target.hasClass('burger') || $target.hasClass('fa-bars')){
+    $('html').toggleClass('open');
+  }
+
+  if($target.hasClass('close')){
+    $('html').removeClass('open');
+  }
+
+});
+
+
+/////////////////////////////////////////
+
+var paginator = function($el){
+  var $lis = $el.children('article'),
+      lisPerPage = 4,
+      cursor = 0,
+      totalPages = Math.ceil( $lis.length / lisPerPage ),
+      turn = function(i){
+        if(i < totalPages && i > -1){
+          cursor = i;
+          var start = cursor*lisPerPage,
+              end = start+lisPerPage;
+          $lis.hide().slice(start,end).show();
+          $pagination.removeClass('active').eq(cursor+1).addClass('active');
+        }
+      };
+
+  if(lisPerPage < $lis.length){
+    var t = _.template($('#paginator').html());
+    var $pagination = $(t({
+      pages: totalPages,
+      cursor: cursor
+    })).insertAfter($el).find('li').on('click', function(e){
+      e.preventDefault();
+      var $this = $(this),
+          i = $this.index()-1;
+      if($this.is(':first-child')){
+        i = cursor-1;
+      } else if($this.is(':last-child')){
+        i = cursor+1;
+      }
+      turn(i);
+    });
+    turn(cursor);
+  }
+}
+
+////////
+
+PubSub.subscribe('Page Built', function(msg, data){
+  $(".paginate").each(function(){
+    new paginator($(this));
+  });
 });
